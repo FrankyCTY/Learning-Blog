7b1117ff79a6497b6c6a429e662d37645345833e
TakYuChan <frankytakyuwork@gmail.com>
2020-09-04 10:00:18 +1200

    added something!

--- app.js
+++ app.js
@@ -8,19 +8,3 @@ menu.addEventListener("click", () => {
     nav.classList.remove("opened");
   }
 });
-
-// Helper: Reads an array of IDs from a collection concurrently
-const readIds = async (collection, ids) => {
-  const readPromises = ids.map((id) => collection.doc(id).get());
-  const snapshots = await Promise.all(readPromises);
-  return snapshots.map((snapshot) => snapshot.data());
-};
-
-
-const getLikedBooks = async() {
-  const userLikesPromises = await firestore.collection('likes').orderBy('jeff').get();
-  const bookIds = userLikesPromises.docs.map(userlikePromise => userlikePromise.id);
-  
-  const likedBooks = bookIds.map(bookId => firestore.collection('books').doc(bookId).get());
-  const books = Promise.all(likedBooks);
-}
